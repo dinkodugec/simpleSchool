dbaded093abeb46f7132d4b9537ba71f08eca501
Dinko <dinko.dugec@gmail.com>
2023-01-03 22:19:39 +0100

    destroy session when user log out

--- public/index.php
+++ public/index.php
@@ -58,6 +58,7 @@ $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 $router->add('login', ['controller'=>'Login', 'action'=>'new']);
+$router->add('logout', ['controller' => 'Login', 'action' => 'destroy']); //automatic destroy session
 
 
 
