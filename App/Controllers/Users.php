254d5d704b172f9f673c4adc1427a51978423ac5
Dinko <dinko.dugec@gmail.com>
2023-09-12 00:06:32 +0200

    if user.uloga == null promjeni u nastavnika

--- App/Controllers/Users.php
+++ App/Controllers/Users.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Auth;
 use App\Models\User;
 use \Core\View;
 
@@ -35,6 +36,11 @@ class Users extends \Core\Controller
      */
     public function promjeniUNastavnikaAction()
     {
+
+      if( !Auth::isAdmin()){
+        $this->redirect('/public/index.php');
+      } 
+
          $id = $_GET['id'];
 
          User::promjeniUNastavnika($id);
